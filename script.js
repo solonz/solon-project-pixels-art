c589fa44bee5a6a707e1d79e1cf752cf91d6f89a
Solon <amaral.solon@hotmail.com>
2022-04-22 14:32:56 -0300

    Concluido requisitos 3 e 4 - Feitos de maneira rocambólica :(

---
+++ script.js
@@ -0,0 +1,23 @@
+// Criamos uma linha de quadrados para cada container
+// let body = document.querySelector('body')
+
+// function criaContainer() {
+//     for (index = 0; index < 5; index++) {
+//         let cont = document.createElement('container')
+//         cont.classList.add('container')
+//         body.appendChild(cont);
+//     }
+// }
+// criaContainer()
+
+// let container = document.querySelector('.container')
+
+// function criaLinha() {
+//     for (index = 0; index < 5; index++) {
+//         let div = document.createElement('div');
+//         div.classList.add('linha');
+//         container.appendChild(div);
+//     }
+// }
+
+// criaLinha()
